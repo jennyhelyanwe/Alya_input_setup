aaf4f7a3b92664ce870dc685e5e782cf982a7148
Jenny Wang <jenny.helyanwe@gmail.com>
2023-06-16 15:37:37 +0100

    Plotting second order effects

--- cellularSA_torordland/cellular_sa.py
+++ cellularSA_torordland/cellular_sa.py
@@ -21,9 +21,28 @@ elif flag == 'analyse':
     Y = np.loadtxt('endo_output.txt', float, delimiter=',', skiprows=1)
     print(Y.shape)
     sp.set_results(Y)
-    sp.analyze_sobol(print_to_console=True, calc_second_order=True)
-    sp.heatmap()
+    sp.analyze_sobol(print_to_console=False, calc_second_order=True)
+    analysis = sp._analysis
+    for str in sp.get('outputs'):
+        print(str)
+        print(analysis[str]['S2'])
+        ax = sns.heatmap(analysis[str]['S2'])
+        ax.set_yticklabels(sp.get('names'), rotation=45, fontsize=14)
+        ax.set_xticklabels(sp.get('names'), rotation=45,
+                           horizontalalignment='right', fontsize=14)
+        ax.set_title(str)
+        plt.show()
+        plt.savefig(str+'_S2.png')
+        sp.plot()
+        plt.savefig(str+'_S1.png')
+        plt.show()
+    #print(sp[0][2]['S2'])
+    # ax = sns.heatmap(analysis[0]['S2']) #, vmin=-1, vmax=1, center=0,
+                     # square=True, annot_kws={"size": 20, "color": "k"}) # cmap=sns.diverging_palette(220, 20, n=200),
+    # for (j, i), label in np.ndenumerate(sp.get('names')):
+    #     ax.text(i + 0.5, j + 0.5, np.round(label, 2),
+    #             fontdict=dict(ha='center', va='center', color='k', fontsize=10))
+    # sns.pairplot(dataframe_ecg_biomarkers)
 
-    sp.plot()
-    plt.show()
+    #sp.plot()
 
