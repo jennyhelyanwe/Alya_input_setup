7358375f2137f7ae10135c5fecdf79754bc27ea5
Jenny Wang <jenny.helyanwe@gmail.com>
2023-08-21 11:48:04 +0100

    Calibration criteria update

--- rodero_postprocess.py
+++ rodero_postprocess.py
@@ -2,7 +2,7 @@ from meshpreprocessing import MeshPreprocessing
 from generatefields import FieldGeneration
 from calibratecv import CalibrateCV
 from alyaformat import AlyaFormat
-from postprocessing import PostProcessing, run
+from postprocessing import PostProcessing
 from sensitivityanalysis import SA
 import os
 import numpy as np
@@ -25,19 +25,31 @@ clinical_data_dir = meta_data_dir + 'clinical_data/'
 verbose = False
 
 ########################################################################################################################
-# Step 1: Run Alya simulation and post-processing
+# # Step 1: Run Alya simulation and post-processing
 if system == 'jureca':
     simulation_dir = '/p/project/icei-prace-2022-0003/wang1/Alya_pipeline/alya_simulations/rodero_baseline_simulation_em_rodero_05_fine/'
 elif system == 'heart':
     simulation_dir = '/users/jenang/Alya_setup_SA/alya_csv/rodero_' + mesh_number + '/'
-run(simulation_dir)
+personalisation_data_dir = meta_data_dir + 'results/personalisation_data/rodero_'+mesh_number+'/'
+alya = AlyaFormat(name=simulation_name, geometric_data_dir=geometric_data_dir,
+                  personalisation_dir=personalisation_data_dir, clinical_data_dir=clinical_data_dir,
+                  simulation_dir = simulation_dir, verbose=verbose)
 
 ########################################################################################################################
 # Step 2: Read in post-processed Alya files
 simulation_json_file = 'rodero_baseline_simulation_em.json'
-pp = PostProcessing(name=simulation_name, geometric_data_dir=geometric_data_dir, simulation_json_file=simulation_json_file,
+pp = PostProcessing(alya=alya, simulation_json_file=simulation_json_file,
                     alya_output_dir=simulation_dir, verbose=verbose)
-
+beat = 1
+# pp.evaluate_ecg_pv_biomarkers(beat=beat)
+# pp.read_csv_fields('EPSXX', 'scalar')
+# pp.read_csv_fields('EPSYY', 'scalar')
+# pp.read_csv_fields('EPSZZ', 'scalar')
+# pp.read_csv_fields('EPSXY', 'scalar')
+# pp.read_csv_fields('EPSYZ', 'scalar')
+# pp.read_csv_fields('EPSXZ', 'scalar')
+pp.read_csv_fields('DISPL', 'vector')
+pp.evaluate_basal_displacement()
 quit()
 pp.evaluate_ecg_pv_biomarkers(beat=0)
 # pp.save_sa_results(sa_results_dir+'sa_results.csv')
