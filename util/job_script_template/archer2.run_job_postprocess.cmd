1f639c67055ca5e4c6659b3ad91f34c10a09e64f
Jenny <jenny.helyanwe@gmail.com>
2024-10-11 11:24:07 +0100

    Fix postprocessing job submission bug archer2

--- util/job_script_template/archer2.run_job_postprocess.cmd
+++ util/job_script_template/archer2.run_job_postprocess.cmd
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --acount=e769-jennywang
+#SBATCH --account=e769-jennywang
 #SBATCH --job-name=heartpp
 #SBATCH --time=00:20:00
 #SBATCH --nodes=<<computational_nodes>>
@@ -7,7 +7,6 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --qos=short
 #SBATCH --partition=standard
-#SBATCH --export-none
 
 module load PrgEnv-gnu
 module load gcc/11.2.0
